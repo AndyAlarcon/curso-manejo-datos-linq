49347c61e9ba0a0ec9f1a1feec7f9d65691ae54c
ISAndresAlarcon <afalarconf@siesa.com>
2024-09-16 17:28:19 -0500

    feat: crear funciones para lectura e impresión de archivo books.json

--- LinqQueries.cs
+++ LinqQueries.cs
@@ -1,7 +1,17 @@
 public class LinqQueries
 {
+    private List<Book> librosCollection;
     public LinqQueries()
     {
-        
+        this.librosCollection = new List<Book>();
+        using (StreamReader reader = new StreamReader("books.json"))
+        {
+            string json = reader.ReadToEnd();
+            this.librosCollection = System.Text.Json.JsonSerializer.Deserialize<List<Book>>(json, new System.Text.Json.JsonSerializerOptions(){ PropertyNameCaseInsensitive = true});
+        }
+    }
+    public IEnumerable<Book> TodaLaColeccion()
+    {
+        return librosCollection;
     }
 }
--- Program.cs
+++ Program.cs
@@ -1,2 +1,13 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+LinqQueries queries = new LinqQueries();
+
+ImprimirValores(queries.TodaLaColeccion());
+
+void ImprimirValores(IEnumerable<Book> listaLibros)
+{
+    Console.WriteLine("{0,-60} {1,15} {2,15}\n", "Titulo", "N. Paginas", "Fecha publicacion");
+    foreach(var item in listaLibros)
+    {
+        Console.WriteLine("{0,-60} {1,15} {2,15}", item.Title, item.PageCount, item.PublishedDate.ToShortDateString());
+    }
+}
